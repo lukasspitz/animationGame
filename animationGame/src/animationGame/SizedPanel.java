344f44a75665d5f354fc1cbee64975330c79cd2c
Spitzmueller.Lukas <Lukas98sp@gmail.com>
2015-10-20 15:31:58 +0200

    10.commit

--- animationGame/src/animationGame/SizedPanel.java
+++ animationGame/src/animationGame/SizedPanel.java
@@ -1,9 +1,21 @@
 package animationGame;
+import javax.swing.*;
+import java.awt.Dimension;
+public class SizedPanel extends JPanel{
+int preferredWidth=400;
+int preferredHeight=300;
 
-public class SizedPanel {
+public SizedPanel(int w,int h){
+	preferredWidth=w;
+	preferredHeight=h;
+}
+public SizedPanel(){};
+@Override public Dimension getPreferredSize(){
+return new Dimension(preferredWidth, preferredHeight);	
+}
+public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ShowInFrame.show(new SizedPanel());
 
 	}
 
